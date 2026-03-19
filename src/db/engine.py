95bb33cce33c5c24b84384d14fc252a81a62506d
naumov <naumov_@mail.ru>
2026-03-19 18:42:18 +0500

    dev: fix

--- src/db/engine.py
+++ src/db/engine.py
@@ -2,5 +2,5 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 
 from src.config import settings
 
-engine = create_async_engine(settings.database_url, echo=False, pool_size=20, max_overflow=10)
+engine = create_async_engine(settings.database_url, echo=False, pool_size=20, max_overflow=10, pool_pre_ping=True)
 async_session_factory = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
